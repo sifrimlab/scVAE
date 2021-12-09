4c9c3ae6b63b47aa4b550d0c8b845f5ebc196d80
josenachorr <joseignacio.alvira@gmail.com>
2021-12-09 11:45:02 +0100

    Added adaptive learning rate function

--- scVAE.py
+++ scVAE.py
@@ -7,3 +7,18 @@ from tensorflow.keras.layers import Dense, Input, Dropout
 from tensorflow.keras.optimizers import Adam, SGD, RMSprop
 from tensorflow.keras import layers
 from tensorflow.keras.callbacks import LearningRateScheduler
+
+
+
+def step_lr(epoch, lr):
+    """
+    This function takes the current epoch and learning rate during training and
+    divides the learning rate by 5 every 200 epochs. This function serves as
+    the main argument for the LearningRateScheduler class. You don't need to
+    initialize the function arguments
+    """
+
+    if (epoch != 0) & (epoch % 200 == 0): # Change every 200 epochs
+        lr = lr / 5
+
+    return lr
